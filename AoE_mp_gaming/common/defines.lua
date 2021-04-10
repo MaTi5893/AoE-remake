b8fef3798958bca4ef2678d71ce6492931dbbf0d
MaTi5893 <45310906+MaTi5893@users.noreply.github.com>
2021-04-10 17:16:18 +0200

    Fixed gaming

--- AoE_mp_gaming/common/defines.lua
+++ AoE_mp_gaming/common/defines.lua
@@ -672,7 +672,7 @@ pops = {
 	
 	POP_TO_LEADERSHIP = 0.0001, -- how much leadership every 1000 officers gives each day.
 	ARTISAN_MIN_PRODUCTIVITY = 2, -- Minimum efficieny of an artisan
-	SLAVE_GROWTH_DIVISOR = 0, -- Slaves have N times lower growth
+	SLAVE_GROWTH_DIVISOR = 1, -- Slaves have N times lower growth
 	
 	MIL_HIT_FROM_CONQUEST = 4, -- how much militancy grows in a province if taken without being core.
 	LUXURY_CON_CHANGE = 0.001, -- con boost from over-buying luxuary goods
